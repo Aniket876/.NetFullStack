9fdede802164ee8c59281b9998f951c698c8d262
Dhanait <aniket-nitin.dhanait@capgemini.com>
2022-12-28 15:31:50 +0530

    assignment 2 committed

---
+++ OOPAssignment2/OOPAssignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOPAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int empid;
+            string empname;
+            double salary;
+            Console.WriteLine("enter the id: ");
+            empid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the name");
+            empname = Console.ReadLine();
+            Console.WriteLine("enter the salary");
+            salary = Convert.ToDouble(Console.ReadLine());
+            Employee emp = new Employee(empid,empname,salary);
+            emp.CalculateSalary();
+            emp.printdetails();
+            Console.ReadKey();
+
+        }
+    }
+}
